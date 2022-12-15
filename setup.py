515045dd93ee8cc633921795eff33de8400b9db7
karinaphilipp <karinaosp@gmail.com>
2022-12-15 15:51:55 -0300

    CLI and setup.py updated

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(
     name='aceppred',
     version='0.0.1',
     packages=find_packages(),
-    author='Karina Philipp'
+    author='Karina Philipp',
+    entry_points = {
+        'console_scripts': [
+            'acppred-train = acppred.train:main',
+            'acppred-predict = acppred.predict:main'
+        ]
+    }
 )
